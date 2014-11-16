dc9ca636ea6457cbb276720df14576db2c138e20
on <olavnybo@gmail.com>
2014-11-16 19:36:50 +0100

    Revert "adding missing file"
    This reverts commit b00ff858d0914219ccd63e8668f7394cbf07577c.

--- BlogPostsByAuthor/AuthorData/BlogLoginSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BloggPostsByAuthor.AuthorData
-{
-    public class BlogLoginSettings
-    {
-        public string Url { get; set; }
-        public string UserName { get; set; }
-        public string Password { get; set; }
-    }
-}
